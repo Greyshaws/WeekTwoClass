0e5e5b5aeee42d7508f130b433fcdc91ee7d43b2
Gracious Igwe <gracious0901@gmail.com>
2022-04-19 05:37:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeekTwoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
